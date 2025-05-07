2c61b89f5dfbace9856e0790e29a1f666a0f3e29
derlymyleyrincongil@gmail.com <derlymyleyrincongil@gmail.com>
2025-05-07 11:10:08 -0500

    Agregue informacion al archivo conexion

---
+++ sena_mvc/Models/conexion.ts
@@ -0,0 +1,11 @@
+ 
+import { Client } from "../Dependencies/dependencias.ts";
+
+
+export const conexion = await new Client().connect({
+    
+    hostname: "localhost",
+    username: "root",
+    db: "sena_mvc",
+    password: "",
+});
